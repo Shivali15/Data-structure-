75da08f13986f9e4e321739380870155afa4d734
shivali umbarkar <shivali.umbarkar@konverge.ai>
2022-02-23 13:22:48 +0530

    files are added

--- Trie.js
+++ Trie.js
@@ -1,10 +1,10 @@
-/* Trie Data Structure */
+// Trie 
 
 let Node = function() {
 	this.keys = new Map();
-	this.end = false;
+	this.end = false;   // all nodes without star have set send to false 
 	this.setEnd = function() {
-		this.end = true;
+		this.end = true;   // end node is true
 	};
 	this.isEnd = function() {
 		return this.end;
@@ -15,11 +15,11 @@ let Trie = function() {
 
 	this.root = new Node();
 
-	this.add = function(input, node = this.root) {
-		if (input.length == 0) {
+	this.add = function(input, node = this.root) {  // add() to add a word to trie recursion function 
+		if (input.length == 0) {  //input means actual word to be insert in trie  
 			node.setEnd();
 			return;
-		} else if (!node.keys.has(input[0])) {
+		} else if (!node.keys.has(input[0])) {   //check that already there a node with that letter 
 			node.keys.set(input[0], new Node());
 			return this.add(input.substr(1), node.keys.get(input[0]));
 		} else {
@@ -27,7 +27,7 @@ let Trie = function() {
 		};
 	};
 
-	this.isWord = function(word) {
+	this.isWord = function(word) {   // to check if that word is present in trie 
 		let node = this.root;
 		while (word.length > 1) {
 			if (!node.keys.has(word[0])) {
@@ -41,8 +41,8 @@ let Trie = function() {
       true : false;
 	};
 
-	this.print = function() {
-		let words = new Array();
+	this.print = function() {   // print () print all the words that are in trie 
+        		let words = new Array();
 		let search = function(node, string) {
 			if (node.keys.size != 0) {
 				for (let letter of node.keys.keys()) {
@@ -71,7 +71,7 @@ myTrie.add('do');
 myTrie.add('dorm')
 myTrie.add('send')
 myTrie.add('sense')
-console.log(myTrie.isWord('doll'))
-console.log(myTrie.isWord('dor'))
-console.log(myTrie.isWord('dorf'))
-console.log(myTrie.print())
+console.log(myTrie.isWord('doll'))  // doll word is in the trie so :true
+console.log(myTrie.isWord('dor'))   // dor word is not in trie so :false
+console.log(myTrie.isWord('dorf'))   // dorf word is not in trie so : false
+console.log(myTrie.print())// print all word that are added to the trie  
